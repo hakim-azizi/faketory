55ff88e6a0e7fa765281ceef569d67e852a8d26c
hakim-azizi <160917355+hakim-azizi@users.noreply.github.com>
2024-05-14 23:50:51 +0200

    first commit

---
+++ client/src/components/ImagesTest.jsx
@@ -0,0 +1,29 @@
+import "./ImagesTest.css";
+
+function ImagesTest() {
+  return (
+    <div className="images">
+      <figcaption>
+        <img src="images/loto.jpg" alt="loto" height="150" />
+      </figcaption>
+
+      <figcaption>
+        <img src="images/JO 2024.jpg" alt="jo-2024" width="260" />
+      </figcaption>
+
+      <figcaption>
+        <img src="images/pape.jpg" alt="pape" height="150" />
+      </figcaption>
+
+      <figcaption>
+        <img
+          src="images/Train Japon.jpg"
+          alt="train-japon"
+          height="150"
+        />
+      </figcaption>
+    </div>
+  );
+}
+
+export default ImagesTest;
